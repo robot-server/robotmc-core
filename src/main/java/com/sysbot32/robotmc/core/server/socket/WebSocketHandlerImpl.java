5aa1e34e546365f9a78c12bb1ddea3d63565a1cb
Seokjin Yoon <sjy5386@gmail.com>
2022-09-18 22:58:50 +0900

    Add WebSocketHandlerImpl

---
+++ src/main/java/com/sysbot32/robotmc/core/server/socket/WebSocketHandlerImpl.java
@@ -0,0 +1,28 @@
+package com.sysbot32.robotmc.core.server.socket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
+public class WebSocketHandlerImpl implements WebSocketHandler {
+
+    @Override
+    @OnWebSocketConnect
+    public void onConnect(final Session session) {
+
+    }
+
+    @Override
+    @OnWebSocketClose
+    public void onClose(final Session session, final int statusCode, final String reason) {
+
+    }
+
+    @Override
+    @OnWebSocketMessage
+    public void onMessage(final Session session, final String message) {
+    }
+}
